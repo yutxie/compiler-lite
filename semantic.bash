6730d4c3266f6da8a3f163276492dac6736c2283
xxxxxyt <inrence@outlook.com>
2018-04-30 16:53:04 +0800

    update

--- semantic.bash
+++ semantic.bash
@@ -5,5 +5,5 @@
 set -e
 cd "$(dirname "$0")"
 export CCHK="java -classpath ./lib/antlr-4.7.1-complete.jar:./bin Main"
-cat > ./Mx/code/program.txt   # save everything in stdin to program.txt
+cat > ./code/program.txt   # save everything in stdin to program.txt
 $CCHK
