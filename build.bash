d84c017117615e534408d2c50fb03ff20d21762b
xxxxxyt <inrence@outlook.com>
2018-04-30 16:10:05 +0800

    update

--- build.bash
+++ build.bash
@@ -1,6 +1,5 @@
 # this script is called when the judge is building your compiler.
 # no argument will be passed in.
-
 set -e
 cd "$(dirname "$0")"
 mkdir -p bin
--- semantic.bash
+++ semantic.bash
@@ -2,7 +2,6 @@
 # print the compiled source, i.e. asm code, directly to stdout.
 # don't print anything other to stdout.
 # if you would like to print some debug information, please go to stderr.
-
 set -e
 cd "$(dirname "$0")"
 export CCHK="java -classpath ./lib/antlr-4.7.1-complete.jar:./bin Main"
